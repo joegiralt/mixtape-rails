bf5917dd3f3ab98aa9085a39f1f6e591b7bf9b0e
github username <github email address>
2013-07-06 16:17:50 -0400

    first commit added artist controller

---
+++ mixtape/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mixtape::Application.config.secret_key_base = '005c0e427d7adbe6023d3fe9cfe533ed3c939878aa59508493b0796ed1e635910acc74f5bbba3dc0d515c15de9c67e416d9e070aacd8b8cbce4887e9ffb72edd'
---
+++ mixtape/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mixtape::Application.config.session_store :cookie_store, key: '_mixtape_session'
